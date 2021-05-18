c4ea199ac6e9cff717daf9707fcafb4228c7a623
=Shad Musthafa <=>
2021-05-18 12:37:43 +0530

    Detail Page created and data bound to control

--- uimodule/webapp/controller/BaseController.js
+++ uimodule/webapp/controller/BaseController.js
@@ -62,7 +62,7 @@ sap.ui.define(
                 if (sPreviousHash !== undefined) {
                     window.history.back();
                 } else {
-                    this.getRouter().navTo("appHome", {}, true /*no history*/);
+                    this.getRouter().navTo("master", {}, true /*no history*/);
                 }
             }
         });
--- uimodule/webapp/controller/Detail.controller.js
+++ uimodule/webapp/controller/Detail.controller.js
@@ -5,6 +5,15 @@ sap.ui.define(["com/smdev/myUI5App/controller/BaseController"], function (Contro
         onInit: function () {
             this.getRouter().getRoute("detail").attachPatternMatched(this._onDetailMatched, this)
         },
+
+        onEdit: function () {
+            var sPath = this.getView().getElementBinding().getPath(),
+                sSupplierId = sPath.substr(sPath.lastIndexOf('/') + 1);
+            this.getRouter().navTo('edit', {
+                id: sSupplierId
+            }, false);
+        },
+
         _onDetailMatched: function (oEvent) {
             var oArgs = oEvent.getParameter("arguments"),
                 sSupplierId = oArgs.id,
